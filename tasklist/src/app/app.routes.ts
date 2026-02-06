d124c57655c109c775fa9c09d17a2effce306c09
NicolasMCascaes <cascaesnicolas7@gmail.com>
2026-02-05 23:27:11 -0300

    feat: login page implemented and better project organization

--- tasklist/src/app/app.routes.ts
+++ tasklist/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/login/login';
+import { Layout } from './layouts/layout/layout';
+import { Dashboard } from './features/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:"",
+    redirectTo:'login',
+    pathMatch:'full'
+  },
+  {
+    path:'login',
+    component:Login
+  },
+  {
+    path:'',
+    component: Layout,
+    children:[
+      {
+        path:'dashboard',
+        component: Dashboard
+      }
+    ]
+  }
+];
